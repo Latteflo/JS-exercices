4460447a3a4fc42634c7ab57a3709af2725aa32c
flori <sf.simion.f@gmail.com>
2023-05-22 14:44:12 +0200

    print shop exercice done

--- conditions.js
+++ conditions.js
@@ -58,3 +58,14 @@ console.log(nameOfDay(3))
 //A print shop charges 0.12 € the ten first copy, 0.11 € the next 20 and 0.10 € from there.
 //Write an algorithm which given a number of copies and calculates the price
 
+function printShopCopy(numberOfCopies){
+    if (numberOfCopies <= 10){
+        return "€"+ (numberOfCopies * 0.12) 
+    }else if ( numberOfCopies < 10 && numberOfCopies >= 30){
+        return "€"+(numberOfCopies * 0.11) 
+    } else {
+        return "€"+(numberOfCopies * 0.10)
+    }
+}
+
+console.log(printShopCopy(6))
