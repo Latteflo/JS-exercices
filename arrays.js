67c8c2b800149d5b9576bf7a7bd471285eb4e6ec
flori <sf.simion.f@gmail.com>
2023-05-23 11:32:01 +0200

    solved exercice 4

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@
 //Write an algorithm which prints everything in an array.
 
 let everythingPrinted = [" Ana", "are", "mere", "dar", "nu", "are", "pere"]
-console.log(everythingPrinted)
+//console.log(everythingPrinted)
 
 
 //II - maximum
@@ -10,14 +10,26 @@ console.log(everythingPrinted)
 
 let mixedNumbers=[3,5,9,7,5,4,10,6,1,8]
 
-console.log(Math.max(...mixedNumbers))
+//console.log(Math.max(...mixedNumbers))
 
 //III - minimum
 //Write an algorithm which receives an array of integers and prints its minimum.
 
+let mixedNumbersTwo=[3,5,9,7,5,4,10,6,1,8]
+
+//console.log(Math.min(...mixedNumbersTwo))
+
+
 //IV - minimum position
 //Write an algorithm which receives an array of integers and prints the position of its minimum.
+
+let mixedNumbersTree=[3,5,9,7,5,4,10,6,1,8]
+let index = mixedNumbersTree.indexOf(Math.min(...mixedNumbersTree))
+
+console.log(index) // prints the index not the value
 
 //V - ordered array
 //Write a algorithm which receives an array of integers and check if its ordered ascendantly.
 // Print true if the array is ordered, false if it’s not.
+
+
