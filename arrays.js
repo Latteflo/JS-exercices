54338681066ba5b828f8f2fed12853ae91814244
flori <sf.simion.f@gmail.com>
2023-05-23 14:07:13 +0200

    finished the exercices

--- arrays.js
+++ arrays.js
@@ -4,32 +4,45 @@
 let everythingPrinted = [" Ana", "are", "mere", "dar", "nu", "are", "pere"]
 //console.log(everythingPrinted)
 
-
 //II - maximum
 //Write an algorithm which receives an array of integers and prints its maximum.
 
-let mixedNumbers=[3,5,9,7,5,4,10,6,1,8]
+let mixedNumbers = [3, 5, 9, 7, 5, 4, 10, 6, 1, 8]
 
 //console.log(Math.max(...mixedNumbers))
 
 //III - minimum
 //Write an algorithm which receives an array of integers and prints its minimum.
 
-let mixedNumbersTwo=[3,5,9,7,5,4,10,6,1,8]
+let mixedNumbersTwo = [3, 5, 9, 7, 5, 4, 10, 6, 1, 8]
 
 //console.log(Math.min(...mixedNumbersTwo))
-
 
 //IV - minimum position
 //Write an algorithm which receives an array of integers and prints the position of its minimum.
 
-let mixedNumbersTree=[3,5,9,7,5,4,10,6,1,8]
+let mixedNumbersTree = [3, 5, 9, 7, 5, 4, 10, 6, 1, 8]
 let index = mixedNumbersTree.indexOf(Math.min(...mixedNumbersTree))
 
-console.log(index) // prints the index not the value
+//console.log(index) // prints the index not the value
 
 //V - ordered array
 //Write a algorithm which receives an array of integers and check if its ordered ascendantly.
 // Print true if the array is ordered, false if it’s not.
 
+let mixedNumbersFour = [3, 5, 9, 7, 5, 4, 10, 6, 1, 8]
 
+let trueValue = []
+let falseValue = []
+
+function checkOrder(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+console.log(checkOrder(mixedNumbersFour));
