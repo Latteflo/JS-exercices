2a052034106933e15ad131e3dc76e563335774dc
flori <sf.simion.f@gmail.com>
2023-05-23 14:25:19 +0200

    completed exercice 1 and 2

--- strings.js
+++ strings.js
@@ -1,8 +1,28 @@
 //I - concatenation
 //Write a function which receives two strings and returns them as one string.
 
+//function concatenateStrings(string1, string2) {
+//    return string1 + " " + string2;
+//}
+
+//let input1 = prompt("Write a string");
+//let input2 = prompt("Write another string");
+
+//let sentence = concatenateStrings(input1, input2);
+
+//console.log(sentence);
+
 //II - lowercase
 //Write a function which receives a character in uppercase and prints it in lowercase.
+
+function lowercaseTransform(string){
+    return string.toLowerCase()
+}
+let input3 = prompt ("write an uppercase character")
+let lowercaseCharacter=lowercaseTransform(input3)
+
+console.log(lowercaseCharacter)
+
 
 //III - uppercase
 //Write a function which receives a string in lowercase and returns an uppercase sentence.
