3761a7d97585f2b8c6170f0f6777cf4d598c7db0
flori <sf.simion.f@gmail.com>
2023-05-23 14:28:05 +0200

    completed exercice 3

--- strings.js
+++ strings.js
@@ -1,31 +1,39 @@
 //I - concatenation
 //Write a function which receives two strings and returns them as one string.
 
-//function concatenateStrings(string1, string2) {
-//    return string1 + " " + string2;
-//}
+function concatenateStrings(string1, string2) {
+    return string1 + " " + string2;
+}
 
-//let input1 = prompt("Write a string");
-//let input2 = prompt("Write another string");
+let input1 = prompt("Write a string");
+let input2 = prompt("Write another string");
 
-//let sentence = concatenateStrings(input1, input2);
+let sentence = concatenateStrings(input1, input2);
 
-//console.log(sentence);
+console.log(sentence);
 
 //II - lowercase
 //Write a function which receives a character in uppercase and prints it in lowercase.
 
-function lowercaseTransform(string){
+function lowerCaseTransform(string){
     return string.toLowerCase()
 }
 let input3 = prompt ("write an uppercase character")
-let lowercaseCharacter=lowercaseTransform(input3)
+let lowerCaseCharacter=lowerCaseTransform(input3)
 
-console.log(lowercaseCharacter)
+console.log(lowerCaseCharacter)
 
 
 //III - uppercase
 //Write a function which receives a string in lowercase and returns an uppercase sentence.
+
+function upperCaseTransform(string){
+    return string.toUpperCase()
+}
+let input4 = prompt ("write an uppercase character")
+let upperCaseCharacter=upperCaseTransform(input4)
+
+console.log(upperCaseCharacter)
 
 //IV - convert name
 //Write a function which receives a name in this format "Doe, John" an returns it in this format "John Doe"
