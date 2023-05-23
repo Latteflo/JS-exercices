e6498815a2127e5733f6e743cfb7bafa907a9d1f
flori <sf.simion.f@gmail.com>
2023-05-23 15:44:55 +0200

    solved exercice 4

--- strings.js
+++ strings.js
@@ -2,41 +2,50 @@
 //Write a function which receives two strings and returns them as one string.
 
 function concatenateStrings(string1, string2) {
-    return string1 + " " + string2;
+  return string1 + " " + string2
 }
 
-let input1 = prompt("Write a string");
-let input2 = prompt("Write another string");
+let input1 = prompt("Write a string")
+let input2 = prompt("Write another string")
 
-let sentence = concatenateStrings(input1, input2);
+let sentence = concatenateStrings(input1, input2)
 
-console.log(sentence);
+console.log(sentence)
 
 //II - lowercase
 //Write a function which receives a character in uppercase and prints it in lowercase.
 
-function lowerCaseTransform(string){
-    return string.toLowerCase()
+function lowerCaseTransform(string) {
+  return string.toLowerCase()
 }
-let input3 = prompt ("write an uppercase character")
-let lowerCaseCharacter=lowerCaseTransform(input3)
+let input3 = prompt("write an uppercase character")
+let lowerCaseCharacter = lowerCaseTransform(input3)
 
 console.log(lowerCaseCharacter)
-
 
 //III - uppercase
 //Write a function which receives a string in lowercase and returns an uppercase sentence.
 
-function upperCaseTransform(string){
-    return string.toUpperCase()
+function upperCaseTransform(string) {
+  return string.toUpperCase()
 }
-let input4 = prompt ("write an uppercase character")
-let upperCaseCharacter=upperCaseTransform(input4)
+let input4 = prompt("write an uppercase character")
+let upperCaseCharacter = upperCaseTransform(input4)
 
 console.log(upperCaseCharacter)
 
 //IV - convert name
 //Write a function which receives a name in this format "Doe, John" an returns it in this format "John Doe"
+
+function swapName(name) {
+    let parts = name.split(", ");
+    return parts[1] + " " + parts[0];
+}
+
+let input5 = "Doe, John";
+let swappedName = swapName(input5);
+
+console.log(swappedName);
 
 //V - whitespace
 //Write a function which receives a sentence full of whitespace and returns it without them.
